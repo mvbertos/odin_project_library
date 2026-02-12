307e1a0d614e1faea739478cc00ea3da7676d6a2
mvbertos <matheus.bertos2@gmail.com>
2026-02-12 20:23:24 -0300

    added a simple cart with a button to update the read status.

--- script.js
+++ script.js
@@ -24,6 +24,48 @@ addBookToLibrary('Lord of the rings', 'J.R.R Tolkien', '500', 'true');
 addBookToLibrary('Percy jackson', 'Rick Riordan', '500', 'true');
 addBookToLibrary('Moby dick', ' Herman Melville ', '500', 'true');
 
-myLibrary.forEach(function (book) {
-    console.log(book.info());
-});
+
+let libraryEl = document.getElementById("library");
+
+function refresh() {
+    libraryEl.replaceChildren();
+    myLibrary.forEach(function (book) {
+        libraryEl.appendChild(_createBookCard(book));
+    });
+}
+
+function _createBookCard(book) {
+    let card = document.createElement("div");
+    card.className = "Card";
+
+    // title and author
+    let titleEl = document.createElement("p");
+    titleEl.textContent = book.title + " by " + book.author
+    card.appendChild(titleEl);
+
+    // pages
+    let pagesEl = document.createElement("p");
+    pagesEl.textContent = "pages:" + book.pages;
+    card.appendChild(pagesEl);
+
+    // Read button
+    let readButtonEl = document.createElement("button");
+    readButtonEl.textContent = book.read ? "read" : "not read"
+    readButtonEl.addEventListener("click", () => {
+        _setBookStatus(book.title, !book.read);
+        refresh();
+    });
+    card.appendChild(readButtonEl);
+
+    return card;
+}
+
+function _setBookStatus(book_title = "", new_status = false) {
+    const book = myLibrary.find((b) => b.title == book_title);
+    if (book != null) {
+        book.read = new_status;
+        console.log(book.read);
+    }
+}
+
+refresh();
